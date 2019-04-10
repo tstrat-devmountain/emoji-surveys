b5671b46a33108158337dc585a88980cf4f770fd
tstrat <tstrat@email.arizona.edu>
2019-04-10 16:11:21 -0700

    Added basic header with logo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import logo from "../../media/devmtn.png";
+export default class Header extends Component {
+  /*
+        componentDidMount to get session?
+    */
+
+  render() {
+    return (
+      <StyledHeader className="header">
+        <StyledLogo src={logo} />
+      </StyledHeader>
+    );
+  }
+}
+
+const StyledHeader = styled.header`
+  height: 100px;
+  width: 100%;
+  background-color: #444;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 2%;
+`;
+const StyledLogo = styled.img`
+  height: 80%;
+`;
