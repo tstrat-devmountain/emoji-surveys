40f621152229af02d4b3a81e1a474e8985dccf66
tstrat <tstrat@email.arizona.edu>
2019-04-29 09:10:35 -0700

    Added init and basic db querries for backend

---
+++ db/init/class_schedule_init.sql
@@ -0,0 +1,79 @@
+insert into schedule ( week, day, topic )
+values
+(1, 1, 'Git/Github, Command Line'),
+(1, 2, 'Javascript I'),
+(1, 3, 'Javascript II'),
+(1, 4, 'HTML/CSS I'),
+(1, 5, 'Javascript III'),
+
+(2, 1, 'Vanilla JS I'),
+(2, 2, 'HTML/CSS II'),
+(2, 3, 'Vanilla JS II'),
+(2, 4, 'Skills Check I'),
+(2, 5, 'Javascript IV'),
+
+(3, 1, 'React I'),
+(3, 2, 'React II'),
+(3, 3, 'React III'),
+(3, 4, 'React IV'),
+(3, 5, 'Node I'),
+
+(4, 1, 'Node II'),
+(4, 2, 'Debugging'),
+(4, 3, '3/4 Stack Review'),
+(4, 4, 'NoDB Project'),
+(4, 5, 'NoDB Project'),
+
+(5, 1, 'NoDB Project'),
+(5, 2, 'React V'),
+(5, 3, 'SQL I'),
+(5, 4, 'Node III'),
+(5, 5, 'Skills Check II'),
+
+(6, 1, 'React VI'),
+(6, 2, 'Node IV'),
+(6, 3, 'SQL II'),
+(6, 4, 'Node V'),
+(6, 5, 'Skills Check III'),
+
+(7, 1, 'HTML/CSS III'),
+(7, 2, 'Javascript V'),
+(7, 3, 'React VII'),
+(7, 4, 'Full Stack Review'),
+(7, 5, 'Personal Project'),
+
+(8, 1, 'Personal Project'),
+(8, 2, 'Personal Project'),
+(8, 3, 'Personal Project'),
+(8, 4, 'Personal Project'),
+(8, 5, 'Personal Project'),
+
+(9, 1, 'Personal Project'),
+(9, 2, 'Personal Project'),
+(9, 3, 'Personal Project'),
+(9, 4, 'Personal Project'),
+(9, 5, 'Personal Project'),
+
+(10, 1, 'Personal Project'),
+(10, 2, 'Personal Project'),
+(10, 3, 'Team Development'),
+(10, 4, 'Javascript VI'),
+(10, 5, 'Javascript VII/ Skills Check IV'),
+
+(11, 1, 'Advanced React I'),
+(11, 2, 'Advanced React II'),
+(11, 3, 'TBD'),
+(11, 4, 'Testing / Group Project Kickoff'),
+(11, 5, 'Group Project'),
+
+(12, 1, 'Group Project'),
+(12, 2, 'Group Project'),
+(12, 3, 'Group Project'),
+(12, 4, 'Group Project'),
+(12, 5, 'Group Project'),
+
+(13, 1, 'Group Project'),
+(13, 2, 'Group Project'),
+(13, 3, 'Group Project'),
+(13, 4, 'Group Project'),
+(13, 5, 'Group Project');
