efbb523eff79e9bbb0cbcd58e116f078b1d58fa2
tstrat <tstrat@email.arizona.edu>
2019-05-07 09:36:36 -0700

    added structure to auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,1 +1,8 @@
-module.exports = {};
+module.exports = {
+    login: (req, res) => {},
+    register: (req, res) => {},
+    logout: (req, res) => {
+        req.session.destroy();
+        res.status(200).send("Logout Successful");
+    }
+};
